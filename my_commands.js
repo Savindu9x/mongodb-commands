813b479474c2cc5e82a89a3e0694a4482d6d5cb4
Pasan Savindu <75357109+Savindu9x@users.noreply.github.com>
2024-05-19 09:04:22 +0530

    Update my_commands.js

--- my_commands.js
+++ my_commands.js
@@ -1,1 +1,22 @@
+// Last Updated: 2024-05-19
 
+show dbs
+
+use admin
+
+db.dropDatabase()
+
+db.createCollection("restaurants")
+
+db.restaurants.insertOne({name: "Tyler", Age:30, GPA:3.3})
+
+db.restaurants.insertMany([])
+
+// retrieve all in collection
+db.restaurants.find({query},{projection})
+
+// sort 1-ascending, -1 > descending
+db.restaurants.find({borough: 'Brooklyn'},{name:1, _id:0}).sort({restaurant_id: 1}).limit(2)
+
+// update single document
+db.restaurants.updateOne({name:"Rivirea Caterer"}, {$set:{cuisine: "Donuts"}})
